9541aca712870bc07bd91d29f2cd1b4eb219c3a5
shuta <baw.keepon69@gmail.com>
2023-01-02 14:44:11 +0900

    🎉 setup eslint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [state] = useState();
+  useEffect(() => {
+    if (state) return;
+  }, [state]);
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
